58519ad9381f5f7f2880256d71a559916fd2c8db
hugo <hblimaafonso@gmail.com>
2024-04-01 12:51:16 -0300

    Início dos estudos de JS

---
+++ aula-10/index.js
@@ -0,0 +1,10 @@
+let nome = "Hugo"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("Insira seu  nome: ")
+// let confirmacao = window.confirm("Você deseja continuar?")
+// alert("Olá mundo!")
---
+++ exercicio-01/index.js
@@ -0,0 +1,11 @@
+let primeiroNome = window.prompt('Digite seu primeiro nome:');
+let sobrenome = window.prompt('Digite seu sobrenome:');
+let campoDeEstudo = window.prompt('Digite seu campo de estudo:');
+let anoDeNascimento = window.prompt('Digite seu ano de nascimento:');
+
+alert('INFORMAÇÕES DO RECRUTA COLETADAS.\n\n Nome: ' + primeiroNome + ' ' + sobrenome + '\n Campo de estudo: ' + campoDeEstudo + '\n Idade: ' + (2024 - anoDeNascimento) + ' anos');
+
+let x = window.prompt('Insira o primeiro valor: ');
+let y = window.prompt('Insira o segundo valor: ');
+
+alert('4 OPERAÇÕES BÁSICAS.\n\n Soma: ' + (parseInt(x) + parseInt(y)) +'\n Subtração: '+ (parseInt(x) - parseInt(y)) +'\n Multiplicação: '+ (parseInt(x) * parseInt(y)) +'\n Divisão: '+ (parseInt(x) / parseInt(y)));
